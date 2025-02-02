585155baf42f75603e8fb0a98a686c07276037a8
Divyanshi Tripathi <divyanshi@hyperthinksys.com>
2025-02-02 04:39:57 +0400

    Chnages in date day number 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/WeatherApp/",
   server: {
     cors: true,
     host: true, // Allows external access
